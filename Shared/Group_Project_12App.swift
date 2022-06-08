6214e33e88ffa68e15717bf6eff978f399e80e03
Evelyngyl <934336856@qq.com>
2022-06-08 14:14:00 -0700

    Initial Commit

---
+++ Shared/Group_Project_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Group_Project_12App.swift
+//  Shared
+//
+//  Created by 葛聿泠 on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Group_Project_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
