f9f9bc0cc6cfabd7162485e9cf93ec336cdc90da
EddieR23 <eddieromero23@csu.fullerton.edu>
2022-06-13 12:47:36 -0700

    2nd commit

--- Shared/Filter.swift
+++ Shared/Filter.swift
@@ -2,11 +2,25 @@
 //  Filter.swift
 //  Group Project 12
 //
-//  Created by Jose Vazquez Reyes on 6/8/22.
+//  Created by Eduardo Romero on 6/8/22.
 //
 
 import Foundation
 
-struct Filter {
-    // add code
+class Filter {
+    var filterType: String
+    var distance: Int
+    
+    init(_ filterType: String, _ distance: Int) {
+        self.filterType = filterType
+        self.distance = distance
+    }
+    
+    func isIncluded(_ filterType: String) -> Bool {
+        // Checks if filter name being searched is valid
+    }
+    
+    func distanceRange(_ distance: String) {
+        // returns list of restaurants within the given distance range
+    }
 }
